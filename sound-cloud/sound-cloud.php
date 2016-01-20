431594f70c28a8a6bd4871c2a449da3428aa9054
Jude Chavez <Chavezjude7@gmail.com>
2016-01-20 09:19:27 -0800

    added gitignore

--- sound-cloud/sound-cloud.php
+++ sound-cloud/sound-cloud.php
@@ -13,7 +13,9 @@
 	<h4>Josh</h4>
 	<body>
 		<p>
-			Josh is an aspiring dj/producer in his early to mid twenties. Josh makes music on his comp
+			Josh is an aspiring dj/producer in his early to mid twenties. Josh makes music on his computer. Josh has found it difficult to distribute his music and get it out to a larger audience. Josh also wishes to be able to showcase things that he has been working on and his musical tastes. Some things that he would like to be able to do is create various playlists and upload any recent dj sets or tracks that he has made. Josh wishes to expand his audience so that he can get booked for shows and events and ultimately get more business as a dj/producer.
 		</p>
 	</body>
+	<h4>Use Case</h4>
+
 </html>
