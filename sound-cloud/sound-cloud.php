7bc6e4bc250fc1fa564db4dfbb3a187b22aee109
Jude Chavez <Chavezjude7@gmail.com>
2016-01-20 09:12:22 -0800

    added gitignore

--- sound-cloud/sound-cloud.php
+++ sound-cloud/sound-cloud.php
@@ -6,7 +6,14 @@
 		</title>
 	</head>
 	<h1>Sound Cloud Playlist Data Relationship Model</h1>
+	<div>
+		<img src="Diagram.svg" alt="not found" >
+	</div>
+	<h2>Typical Persona</h2>
+	<h4>Josh</h4>
 	<body>
-
+		<p>
+			Josh is an aspiring dj/producer in his early to mid twenties. Josh makes music on his comp
+		</p>
 	</body>
 </html>
