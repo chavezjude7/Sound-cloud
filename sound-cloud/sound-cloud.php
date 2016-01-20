bb55ec937897573d86e4edafe7b70a60d6faf4e2
Jude Chavez <Chavezjude7@gmail.com>
2016-01-20 09:39:13 -0800

    added gitignore

--- sound-cloud/sound-cloud.php
+++ sound-cloud/sound-cloud.php
@@ -10,6 +10,18 @@
 		<img src="Diagram.svg" alt="not found" >
 	</div>
 	<h2>Purpose, Audience, Goal</h2>
+	<h3>Purpose</h3>
+	<body>
+		To allow artists to listen to and collaborate on audio media.
+	</body>
+	<h3>Audience</h3>
+	<body>
+		Artists and music producers looking to expand their listener base.
+	</body>
+	<h3>Goal</h3>
+	<body>
+		To expand listener base and distribution.
+	</body>
 	<h3>Typical Persona</h3>
 	<h4>Josh</h4>
 	<body>
@@ -17,7 +29,7 @@
 			Josh is an aspiring dj/producer in his early to mid twenties. Josh makes music on his computer. Josh has found it difficult to distribute his music and get it out to a larger audience. Josh also wishes to be able to showcase things that he has been working on and his musical tastes. Some things that he would like to be able to do is create various playlists and upload any recent dj sets or tracks that he has made. Josh wishes to expand his audience so that he can get booked for shows and events and ultimately get more business as a dj/producer.
 		</p>
 	</body>
-	<h4>Use Case</h4>
+	<h3>Use Case</h3>
 	<body>
 		<p>For this particular website Josh will have to create a profile. Once Josh Creates his profile he will have to add tracks. Once he has done that josh will have to name and describe his track along with added specific hastags in order to categorize his music within the site. In order to create a playlist josh will have to like a track in order to added it to a playlist of his choosing. If someone wants to add his tracks to there playlist they will have to do the same. </p>
 	</body>
